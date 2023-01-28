222799237043821e6dfcf27651fbf07d2b14bc21
Rayhanuc <rayhanctg4@gmail.com>
2023-01-29 03:49:00 +0600

    jQuery selector

---
+++ selectors/assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#list').css('color','#561c66');
+  // $('#list').css('border','2px solid red');
+  // $('*').css('border','2px solid red');
+  // $('li:second-child').css('border','2px solid red');
+  $('ul li:eq(1)').css('border','2px solid red');
+});
