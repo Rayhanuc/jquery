996b8b8bd9caac1b0b2f65cc0b62071cb72744e4
Rayhanuc <rayhanctg4@gmail.com>
2023-07-26 02:42:05 +0600

    Mouse event use in jQuery

---
+++ mouse_events2/assets/js/main.js
@@ -0,0 +1,25 @@
+// $(document).ready(function() {
+//   // $("#box").click(function () {
+//   //   // var a = $("#box").html();
+//   //   // console.log(a);
+//   // });
+//   $("#box").click(function () {
+//     $("#box").css("background-color", "#61bcc8");
+//   });
+
+//   $("#box").dblclick(function () {
+//     $("#box").css({"background-color": "#ebebeb", "color": "black", "transition": "500ms"});
+//   });
+
+//   $("#box").contextmenu(function () {
+//     $("#box").css({"background-color": "orange", "color": "black", "transition": "500ms"});
+//   });
+
+//   $("#box").mouseenter(function () {
+//     $("#box").css({"background-color": "red", "color": "white", "transition": "500ms"});
+//   });
+
+//   $("#box").mouseleave(function () {
+//     $("#box").css({"background-color": "purple", "color": "yellow", "transition": "500ms"});
+//   });
+// });
